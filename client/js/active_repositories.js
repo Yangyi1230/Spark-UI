bca8f2fcb2b334e689190306851168ec1bb02251
Spikerman <spikechenhao@163.com>
2018-04-20 17:57:03 -0400

    mongodb - server - client connected

--- client/js/active_repositories.js
+++ client/js/active_repositories.js
@@ -16,45 +16,51 @@ import Chart from '../../node_modules/chart.js/src/chart';
 // });
 
 
+const collection = new Mongo.Collection("output");
 
-
-//TODO: Dynamically load data from database
+//TODO bind data to chart after retrieve all data
 Template.ActiveRepositories.onCreated(function () {
-    const languages = new Mongo.Collection("output");
     this.autorun(() => {
         this.subscribe('languages');
-        let results = languages.find();
+        let results = collection.find({language: "javascript"}, {sort: {time: 1}});
         results.forEach((result) => {
             console.log(`time: ${result.time}, language: ${result.language}, score: ${result.score}`);
+
+        });
+
+        let times = [];
+        let scores = [];
+        results.forEach((result) => {
+            times.push(result.time);
+            scores.push(result.score);
+        });
+
+        let ctx = document.getElementById("Active-Repos");
+
+        let myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: times,
+                datasets: [{
+                    data: scores,
+                    label: "javascript",
+                    borderColor: "#3e95cd",
+                    fill: false
+                }]
+            },
+            options: {
+                title: {
+                    display: false,
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: '# of active repos'
+                        }
+                    }]
+                }
+            }
         });
     });
-
-
-    // let ctx = document.getElementById("Active-Repos");
-    //
-    // let myChart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //         labels: ['16/Q1', '16/Q2', '16/Q3', '16/Q4', '17/Q1', '17/Q2', '17/Q3', '17/Q4'],
-    //         datasets: [{
-    //             data: [86, 114, 106, 106, 107, 111, 133, 221],
-    //             label: "C++",
-    //             borderColor: "#3e95cd",
-    //             fill: false
-    //         }]
-    //     },
-    //     options: {
-    //         title: {
-    //             display: false,
-    //         },
-    //         scales: {
-    //             yAxes: [{
-    //                 scaleLabel: {
-    //                     display: true,
-    //                     labelString: '# of active repos'
-    //                 }
-    //             }]
-    //         }
-    //     }
-    // });
 });
--- server/main.js
+++ server/main.js
@@ -2,9 +2,10 @@
 //Email : dayoudu@nyu.edu
 import {Meteor} from 'meteor/meteor';
 
+
+//TODO: load data from local mongodb
 Meteor.startup(() => {
     const languages = new Mongo.Collection("output");
-    languages.insert({time: '2014-3', language: 'emacs', score: 1});
     Meteor.publish('languages', function () {
         return languages.find();
     });
