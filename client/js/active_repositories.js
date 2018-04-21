d592be0c444115f2b57891c06cd9c7f3616ec1ce
Spikerman <spikechenhao@163.com>
2018-04-20 20:27:56 -0400

    bug fix, add graph render process into callback function

--- client/js/active_repositories.js
+++ client/js/active_repositories.js
@@ -2,65 +2,61 @@
 //Email : dayoudu@nyu.edu
 import Chart from '../../node_modules/chart.js/src/chart';
 
-
-// const handle = Meteor.subscribe('collection');
-//
-// Tracker.autorun(() => {
-//     const isReady = handle.ready();
-//     if (isReady) {
-//         let results = handle.find({"language": "javascript"});
-//         results.forEach((result) => {
-//             console.log(`time: ${result.time}, language: ${result.language}, score: ${result.score}`);
-//         });
-//     }
-// });
-
-
 const collection = new Mongo.Collection("output");
 
-//TODO bind data to chart after retrieve all data
+
 Template.ActiveRepositories.onCreated(function () {
-    this.autorun(() => {
-        this.subscribe('languages');
-        let results = collection.find({language: "javascript"}, {sort: {time: 1}});
-        results.forEach((result) => {
-            console.log(`time: ${result.time}, language: ${result.language}, score: ${result.score}`);
+});
 
-        });
 
-        let times = [];
-        let scores = [];
-        results.forEach((result) => {
-            times.push(result.time);
-            scores.push(result.score);
-        });
+//TODO bind data to chart after retrieve all data
+Template.ActiveRepositories.onRendered(function () {
+    Meteor.subscribe('languages', {
+        onReady: function () {
+            let results = collection.find({language: "javascript"}, {sort: {time: 1}});
+            results.forEach((result) => {
+                console.log(`time: ${result.time}, language: ${result.language}, score: ${result.score}`);
+            });
+            let times = [];
+            let scores = [];
+            results.forEach((result) => {
+                times.push(result.time);
+                scores.push(result.score);
+            });
 
-        let ctx = document.getElementById("Active-Repos");
+            console.log(times.length);
+            console.log(scores.length);
 
-        let myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: times,
-                datasets: [{
-                    data: scores,
-                    label: "javascript",
-                    borderColor: "#3e95cd",
-                    fill: false
-                }]
-            },
-            options: {
-                title: {
-                    display: false,
-                },
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: '# of active repos'
-                        }
+            let ctx = document.getElementById("Active-Repos");
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: times,
+                    datasets: [{
+                        data: scores,
+                        label: "javascript",
+                        borderColor: "#3e95cd",
+                        fill: false
                     }]
+                },
+                options: {
+                    title: {
+                        display: false,
+                    },
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Overall Rating'
+                            }
+                        }]
+                    }
                 }
-            }
-        });
+            });
+        },
+        onError: function () {
+            console.log("onError", arguments);
+        }
     });
 });
