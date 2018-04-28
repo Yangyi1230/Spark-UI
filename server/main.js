7629408336de9ffb39dc6899a3bbb655656be1ac
Spikerman <spikechenhao@163.com>
2018-04-28 16:27:52 -0400

    UI complete II (Table JS need to be completed)

--- server/main.js
+++ server/main.js
@@ -5,8 +5,32 @@ import {Meteor} from 'meteor/meteor';
 
 //TODO: load data from local mongodb
 Meteor.startup(() => {
-    const technology = new Mongo.Collection("Tech");
+    const techCollection = new Mongo.Collection("Tech");
+    const languageCollection = new Mongo.Collection("LangCombined");
+    const issueCollection = new Mongo.Collection("NumIssue");
+    const prCollection = new Mongo.Collection("NumPR");
+    const postCollection = new Mongo.Collection("NumPost");
+    const pushCollection = new Mongo.Collection("NumPush");
+    const starCollection = new Mongo.Collection("NumStar");
     Meteor.publish('techTable', function () {
-        return technology.find();
+        return techCollection.find();
+    });
+    Meteor.publish('languageTable', function () {
+        return languageCollection.find();
+    });
+    Meteor.publish('issueTable', function () {
+        return issueCollection.find();
+    });
+    Meteor.publish('prTable', function () {
+        return prCollection.find();
+    });
+    Meteor.publish('postTable', function () {
+        return postCollection.find();
+    });
+    Meteor.publish('pushTable', function () {
+        return pushCollection.find();
+    });
+    Meteor.publish('starTable', function () {
+        return starCollection.find();
     });
 });
