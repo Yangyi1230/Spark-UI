5b089f446eb589205018e123722ad72cd4ce1128
Ubuntu <ubuntu@ip-172-31-5-237.us-west-1.compute.internal>
2018-05-02 22:43:41 +0000

    two color looks too similar..now it looks beter:P

--- client/js/line_color.js
+++ client/js/line_color.js
@@ -1,3 +1,3 @@
-const colorPicker = ["#001f3f", "#0074D9", "#7FDBFF", "#39CCCC", "#3D9970", "#2ECC40", "#01FF70", "#FFDC00", "#FF851B", "#FF4136", "#F012BE", "#B10DC9", "#111111", "#AAAAAA", "#DDDDDD"];
+const colorPicker = ["#5042F4", "#0074D9", "#7FDBFF", "#39CCCC", "#3D9970", "#2ECC40", "#01FF70", "#FFDC00", "#FF851B", "#FF4136", "#F012BE", "#B10DC9", "#111111", "#AAAAAA", "#DDDDDD"];
 
 export {colorPicker};
